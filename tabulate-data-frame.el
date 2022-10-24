b9e5672f7e9624cbb6dbb8cddf4f578cb23c9b28
John Haman <mail@johnhaman.org>
2022-10-24 16:39:32 -0400

    test

--- tabulate-data-frame.el
+++ tabulate-data-frame.el
@@ -6,7 +6,7 @@
 ;; Maintainer: John Haman <mail@johnhaman.org>
 ;; Created: 2022
 ;; Version: 0.1
-;; Package-Requires: ((emacs "27.1") (ess "18.10.2"))
+;; Package-Requires: ((emacs "27.1") (ess "18.10.2") (consult "0.15"))
 ;; Homepage: https://github.com/jthaman/tabulate-data-frame
 
 ;; This file is not part of GNU Emacs.
@@ -27,18 +27,17 @@
 
 ;;; Commentary:
 
-;; Experimental library to display R data.frames in a new buffer using
-;; `tabulated-list-mode'.
-
 ;;; Todo:
 
 ;; - Make it work in more environments.
+
+(number-to-string (random t))
 
 ;;; Code:
 
 (eval-when-compile
   (require 'cl-lib))
-(require 'tabulated-list)
+(require 'consult)
 (require 'ess-r-mode)
 
 (defgroup tabulate-data-frame ()
@@ -52,15 +51,6 @@
 (defvar list-data-frames-cmd
   "Filter(function(x) is.data.frame(get(x)), ls(envir = .GlobalEnv))"
   "R command to get a list of data.frames from the R global environment.")
-
-(defun get-data-frames ()
-  "Determine the list of data.frame names in the R .GlobalEnv."
-  (ess-command list-data-frames-cmd))
-
-(defun bracket-text-p (string)
-  "Match STRINGs like [1] or [15]."
-  (string-match (rx (or "[" "]"))
-                string))
 
 (defun buffer-to-list (buf)
   "Buffer to list conversion helper function.
@@ -76,96 +66,19 @@ list of data.frame names."
              initial-list))
       (cl-delete-if #'bracket-text-p initial-list))))
 
+(defun get-data-frames ()
+  "Determine the list of data.frame names in the R .GlobalEnv."
+  (ess-command list-data-frames-cmd))
 
-(defun get-df-cmd (df-name)
-  "Generate R code to get the value of the data.frame DF-NAME."
-  (concat "get(deparse(substitute(" df-name ")))"))
+(defun df-file-name (df-name)
+  "Create a temp file-name for the data-frame"
+  (let ((rand-int (number-to-string (random t))))
+    (make-directory (concat browse-url-temp-dir rand-int))
+    (concat browse-url-temp-dir rand-int "/" df-name ".csv")))
 
-;; Need function to create tabulated-list-format from the colnames
-
-(defun get-df-col-names-cmd (df-name)
-  "Return a list of the column names of DF-NAME."
-  (let ((cmd (concat "colnames(" df-name ")")))
-
-    (setq-local ess-dialect "R")
-    (ess-force-buffer-current)
-    (ess-command cmd)
-    (buffer-to-list ess-command-buffer)))
-
-(defun get-df-col-length-cmd (df-name)
-  "Return a list of the column names of DF-NAME."
-  (let ((cmd (concat "c(apply(" df-name ", 2, function(x) max(nchar(x))), use.names = FALSE)")))
-
-    (setq-local ess-dialect "R")
-    (ess-force-buffer-current)
-    (ess-command cmd)
-    (buffer-to-list ess-command-buffer)))
-
-(defun get-df-col-length-cmd (df-name)
-  "Return a list of the column names of DF-NAME."
-  (let ((cmd (concat "c(apply(" df-name ", 2, function(x) max(nchar(x))), use.names = FALSE)")))
-    (ess-force-buffer-current)
-    (ess-command cmd)
-    (buffer-to-list ess-command-buffer)))
-
-(defun make-header (df-name)
-  "Tabulated-data-frame helper function.
-
-Create the input to `tabulated-list-format', the columns and
-spacing for the tabulated-list.
-
-Tabulated-list-format is created dynamically depending on the
-form of DF-NAME in the R session."
-  (let ((header (get-df-col-names-cmd df-name))
-        (col-lengths (get-df-col-length-cmd df-name)))
-    (vconcat
-     (cl-loop for name being each element of header
-              for length being each element of col-lengths
-              collect (list name
-                            (max (string-to-number length) 4) ; columns can't be shorter than 4 chars
-                            t))
-     nil)))
-
-;; Need function to create tabulated-list-entries from the rows of df
-
-(defun get-df-row-names-cmd (df-name)
-  "Return a list of the row names of DF-NAME."
-  (let ((cmd (concat "rownames(" df-name ")")))
-    (setq-local ess-dialect "R")
-    (ess-force-buffer-current)
-    (ess-command cmd)
-    (buffer-to-list ess-command-buffer)))
-
-(defun get-df-data-cmd (df-name)
-  "Tabulated-data-frame helper function.
-
-Read the print out of the data.frame DF-NAME and construct a
-list of vectors: one vector for each row in DF-NAME."
-  (let ((cmd (concat
-              "write.table(" df-name ", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = \"|||\")")))
-    (setq-local ess-dialect "R") ; test to try to remove dialect check
-    (ess-force-buffer-current)
-    (ess-command cmd)
-    (with-current-buffer ess-command-buffer
-      (cl-loop until (eobp)
-               collect (vconcat ; convert list to vector
-                        (split-string ; split line on the space and remove newline
-                         (thing-at-point 'line t) ; take each line of buffer
-                         "|||" nil (rx "\n"))
-                        nil)
-               do (forward-line)))))
-
-
-(defun data-for-table (df-name)
-  "Extract info from DF-NAME for display."
-  (let ((row-names (get-df-row-names-cmd df-name))
-        (df-data (get-df-data-cmd df-name)))
-    (cl-mapcar #'list row-names df-data)))
-
-
-(define-derived-mode tabulate-data-frame-mode tabulated-list-mode "Tabulated Data.frame"
-  "Mode for inspecting data.frames in the R session."
-  :group 'tabulate-data-frame)
+(defun write-temp-data-frame (df-name file)
+  (ess-command
+   (concat "write.csv(" df-name ",\"" file "\")")))
 
 ;;;###autoload
 (defun tabulate-data-frame (df-name)
@@ -180,18 +93,9 @@ data.frames and view it."
      (list
       (completing-read "data.frame> " (buffer-to-list ess-command-buffer)))))
 
-  (with-current-buffer (get-buffer-create
-                        (concat "*R data.frame: " df-name "*"))
-
-    (let ((tabulated-list-entries (data-for-table df-name))
-          (tabulated-list-format (make-header df-name))
-          (tabulated-list-sort-key nil))
-
-      (setq tabulated-list-padding 1)
-      (tabulate-data-frame-mode)
-      (tabulated-list-init-header)
-      (tabulated-list-print t))
-    (display-buffer (current-buffer))))
+  (let ((file (df-file-name df-name)))
+    (write-temp-data-frame df-name file)
+    (consult-file-externally file)))
 
 (provide 'tabulate-data-frame)
 
